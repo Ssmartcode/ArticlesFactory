5c1a1d2f51cb22993316be748973bb2a303b761f
Ssmartcode <razvan.anca.andrei@gmail.com>
2021-03-30 19:36:54 +0300

    Add premium label on articles from family categ and fix bugs

--- routers/articles.js
+++ routers/articles.js
@@ -19,31 +19,34 @@ router.get("/health", async (req, res) => {
   const articles = await Article.find({ category: "health" });
   res.render("category", { articles, category: "health" });
 });
-router.get("/article/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const article = await Article.findById(id);
-    const date = new Date(article.createdAt);
-    const formatedDate = moment(date).format("DD/MM/YYYY-hh:mm:ss a");
-    res.render("article", { article, formatedDate });
-  } catch (err) {
-    req.flash("danger", err.message);
-    res.redirect("/");
-  }
-});
-
-// Premium categories
-router.use((req, res, next) => {
-  if (req.isAuthenticated()) next();
-  else {
+router.get("/family", async (req, res) => {
+  if (req.isAuthenticated()) {
+    const articles = await Article.find({ category: "family" });
+    res.render("category", { articles, category: "family" });
+  } else {
     req.flash("warning", "You need an account to access these articles.");
     res.redirect("/user/login");
   }
 });
 
-router.get("/family", async (req, res) => {
-  const articles = await Article.find({ category: "family" });
-  res.render("category", { articles, category: "family" });
+//Get article from DB
+router.get("/article/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const article = await Article.findById(id);
+    // Redirect if anonym user tries to access a premium article
+    if (article.category === "family" && !req.isAuthenticated()) {
+      req.flash("warning", "You need an account to read this article");
+      res.redirect("/user/login");
+    } else {
+      const date = new Date(article.createdAt);
+      const formatedDate = moment(date).format("DD/MM/YYYY-hh:mm:ss a");
+      res.render("article", { article, formatedDate });
+    }
+  } catch (err) {
+    req.flash("danger", err.message);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
